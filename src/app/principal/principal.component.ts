6c38b0bb299bb81c014b9f2a89ab50ba82a9fa42
unknown <fernandoopaez@gmail.com>
2023-06-22 16:06:59 -0300

    principal.ts en proceso

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { ChartConfiguration, ChartOptions, ChartType } from "chart.js";
 import { BsLocaleService } from 'ngx-bootstrap/datepicker';
+import { CovidService } from '../services/covid.service';
+import { DatePipe } from '@angular/common';
+import { forkJoin } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-principal',
@@ -10,39 +14,85 @@ import { BsLocaleService } from 'ngx-bootstrap/datepicker';
 export class PrincipalComponent {
 
   public lineChartData: ChartConfiguration<'line'>['data'] = {
-    labels: [
-      'January',
-      'February',
-      'March',
-      'April',
-      'May',
-      'June',
-      'July'
-    ],
+    
     datasets: [
-      {
-        data: [ 65, 59, 80, 81, 56, 55, 40 ],
-        label: 'Series A',
-        fill: true,
-        tension: 0.5,
-        borderColor: 'black',
-        backgroundColor: 'rgba(255,0,0,0.3)'
-      }
-    ]
+      {data:[], label:'Confirmados'},
+      {data:[], label:'Recuperados'},
+      {data:[], label:'Activos'},
+      {data:[], label:'Defunciones'},
+
+    ],
+    labels: []
+
   };
+
+  
+
   public lineChartOptions = {
-    responsive: false
+    responsive: true
   };
   public lineChartLegend = true;
+  public lineChartType='Line';
+  public lineChartPlugins=[];
+
 
 
   
   locale= 'es';
 
+  countries:string[]=[];
+  country:string="";
+
+  dateInit: Date | null = null;
+  dateEnd: Date | null = null;
+  
+  minCovDate:Date;
+  maxCovDate:Date;
+
   constructor(
-    private localservice:BsLocaleService
+    private localservice:BsLocaleService,
+    private covidService:CovidService,
+    private datePipe:DatePipe
   ){
     this.localservice.use(this.locale);
+    this.minCovDate= new Date('2020-1-22');
+    this.maxCovDate=new Date();
+    this.maxCovDate.setDate(this.maxCovDate.getDate() -1);
+  }
+
+  ngOnInit():void{
+    this.getCountries();
+  }
+
+  getCountries():void{
+    this.covidService.getAll().subscribe(
+      data =>{
+        this.countries=Object.keys(data);
+      }
+    )
+  }
+
+
+  loadData(event:any):void{
+    if(this.country && this.dateInit && this.dateEnd){
+      forkJoin([
+        this.covidService.twoDates(this.country, this.dateInit, this.dateEnd).pipe(map(data => data.map(val => val.confirmed))),
+        this.covidService.twoDates(this.country, this.dateInit, this.dateEnd).pipe(map(data => data.map(val => val.recovered))),
+        this.covidService.twoDates(this.country, this.dateInit, this.dateEnd).
+        pipe(map(data => data.map(val => val.confirmed - val.recovered -val.deaths))),
+        this.covidService.twoDates(this.country, this.dateInit, this.dateEnd).pipe(map(data => data.map(val => val.deaths))),
+        this.covidService.twoDates(this.country, this.dateInit, this.dateEnd).pipe(map(data => data.map(val => this.datePipe.transform(val.date, 'dd/MM')))),
+      ]).subscribe((
+        [data0,data1,data2,data3,data4]
+      )=>{
+        this.lineChartData.datasets[0].data=data0;
+        this.lineChartData.datasets[1].data=data1;
+        this.lineChartData.datasets[2].data=data2;
+        this.lineChartData.datasets[3].data=data3;
+        this.lineChartData.labels=data4;
+      })
+
+    }
   }
 
 }
---
+++ src/app/services/covid.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators'
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CovidService {
+
+  URLcovid='https://pomber.github.io/covid19/timeseries.json';
+
+  constructor( private http :HttpClient) { }
+
+  public getAll(): Observable<any>{
+    return this.http.get<any>(this.URLcovid);
+  };
+
+  public fromCountry(country: string):Observable<any[]>{
+    return this.getAll().pipe(map(data=> data[country]));
+  }
+
+  public twoDates(country: string, dateFrom:Date,dateTo:Date):Observable<any[]>{
+  return  this.fromCountry(country)
+  .pipe(map(res => res.filter(val => new Date(val.date)>= dateFrom && new Date(val.date)<= dateTo)))
+  }
+
+}
